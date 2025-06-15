2c0102557dc93e6ebfdfffc3c951e5826438449b
Al Wassi Khan <145786993+wassi-real@users.noreply.github.com>
2025-06-15 20:31:42 +0600

    Update supabase-api.js

--- src/lib/supabase-api.js
+++ src/lib/supabase-api.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseServiceKey = import.meta.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
